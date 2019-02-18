087e8613dbf0581ade4871e2b590d97579d44c0f
Stephen Wright <scw248@gmail.com>
2019-02-18 19:47:24 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,14 +1,14 @@
-class Create_Costume_Stores < ActiveRecord::Migration
+# class Create_Costume_Stores < ActiveRecord::Migration
   
-  def change
-    create_table :costume_stores do |t|
-      t.string :name
-      t.string :location
-      t.integer :costume_inventory
-      t.integer :number_of_employees
-      t.boolean :in_business?
-      t.time_stamp :opening_time
-      t.time_stamp :closing_time
-    end
-  end
-end
+#   def change
+#     create_table :costume_stores do |t|
+#       t.string :name
+#       t.string :location
+#       t.integer :costume_inventory
+#       t.integer :number_of_employees
+#       t.boolean :in_business?
+#       t.time_stamp :opening_time
+#       t.time_stamp :closing_time
+#     end
+#   end
+# end
