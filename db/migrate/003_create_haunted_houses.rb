4cfa1d3a374984e3d4d7e9faef7b7909ba396043
Stephen Wright <scw248@gmail.com>
2019-02-18 19:47:29 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,15 +1,15 @@
-class Create_Haunted_Houses < ActiveRecord::Migration
+# class Create_Haunted_Houses < ActiveRecord::Migration
   
-  def change
-    create_table :haunted_houses do |t|
-      t.string :name
-      t.string :location
-      t.string :theme
-      t.integer :price
-      t.boolean :family_friendly?
-      t.date :opening_date
-      t.date :closing_date
-      t.string :long_description
-    end
-  end
-end
+#   def change
+#     create_table :haunted_houses do |t|
+#       t.string :name
+#       t.string :location
+#       t.string :theme
+#       t.integer :price
+#       t.boolean :family_friendly?
+#       t.date :opening_date
+#       t.date :closing_date
+#       t.string :long_description
+#     end
+#   end
+# end
