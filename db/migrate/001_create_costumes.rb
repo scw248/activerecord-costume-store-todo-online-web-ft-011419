5bd53773433dd51f95316b4a64bf3ef88798572e
Stephen Wright <scw248@gmail.com>
2019-02-18 19:32:28 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -3,7 +3,7 @@ class Create_Costumes < ActiveRecord::Migration
   def change
     create_table :costumes do |t|
       t.string :name
-      t.integer :price
+      t.float :price
       t.integer :size
       t.href :image_url
       t.time_stamp :created_at
