3ffeee2e6d21dd95daebbe734b9d0b43180db320
Stephen Wright <scw248@gmail.com>
2019-02-18 19:23:13 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -6,8 +6,7 @@ class Create_Haunted_Houses < ActiveRecord::Migration
       c.string :location
       c.string :theme
       c.integer :price
-      c.integer :number_of_employees
-      c.boolean :in_business?
+      c.boolean :family_friendly?
       c.time_stamp :opening_time
       c.time_stamp :closing_time
     end
