72748bb7766360240623e3c20c0d029d1960a925
Stephen Wright <scw248@gmail.com>
2019-02-18 19:22:04 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,14 @@
-# Create your haunted_houses migration here
+class Create_Costume_Stores < ActiveRecord::Migration
+  
+  def change
+    create_table :costume_stores do |c|
+      c.string :name
+      c.string :location
+      c.integer :costume_inventory
+      c.integer :number_of_employees
+      c.boolean :in_business?
+      c.time_stamp :opening_time
+      c.time_stamp :closing_time
+    end
+  end
+end# Create your haunted_houses migration here
